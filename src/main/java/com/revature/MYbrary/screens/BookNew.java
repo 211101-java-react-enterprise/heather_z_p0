346c219b420f8eb88f26c564b68e74348c2aa84e
Heather <ada@VIIchariot.com>
2021-11-17 08:20:56 -0600

    Heading back to main

--- src/main/java/com/revature/MYbrary/screens/BookNew.java
+++ src/main/java/com/revature/MYbrary/screens/BookNew.java
@@ -1,4 +1,42 @@
 package com.revature.MYbrary.screens;
 
-public class BookNew {
+import com.revature.MYbrary.daos.BookDAO;
+import com.revature.MYbrary.models.Book;
+import com.revature.MYbrary.models.Library;
+import com.revature.MYbrary.services.BookService;
+import com.revature.MYbrary.util.ScreenRouter;
+
+import java.io.BufferedReader;
+
+public class BookNew extends Screen {
+    private Library library;
+    public BookNew(BufferedReader consoleReader, ScreenRouter router, Library library) {
+        super("BookNew", "/books", consoleReader, router);
+        this.library = library;
+    }
+
+    private BookDAO bookDAO = new BookDAO();
+    private BookService bookService = new BookService(bookDAO);
+
+    @Override
+    public void render() throws Exception {
+        System.out.println("Please enter details for your new book.");
+
+        System.out.print("TITLE: ");
+        String title = consoleReader.readLine();
+        System.out.print("AUTHOR: ");
+        String author = consoleReader.readLine();
+        System.out.print("PAGE COUNT: ");
+        Integer pageCount = Integer.parseInt(consoleReader.readLine());
+
+        Book newBook = new Book(title, author, pageCount, library.getId());
+
+        // Library newLibrary = new Library(libraryName, userService.getSessionUser().getId());
+        try {
+            bookService.createNewBook(newBook);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/revature/MYbrary/screens/LibraryScreen.java
+++ src/main/java/com/revature/MYbrary/screens/LibraryScreen.java
@@ -8,6 +8,8 @@ import com.revature.MYbrary.services.LibraryService;
 import com.revature.MYbrary.util.LinkedList;
 import com.revature.MYbrary.util.ScreenRouter;
 
+import static com.revature.MYbrary.util.AppState.shutdown;
+
 import java.io.BufferedReader;
 
 public class LibraryScreen extends Screen {
@@ -22,12 +24,28 @@ public class LibraryScreen extends Screen {
 
     @Override
     public void render() throws Exception {
-        // Display all libraries, then prompt the user to select one.
-        // Then render the specifics
         AppUser user = userService.getSessionUser();
+
         Library activeLibrary = libraryService.promptLibrarySelection(user, consoleReader);
 
         System.out.printf("======== LIBRARY - %s ========", activeLibrary.getName());
+        System.out.print("\n" +
+                " 1) New Book\n" +
+                " 2) View Books\n" +
+                " 3) Exit\n\n" +
+                "> ");
+        String userSelection = consoleReader.readLine();
 
+        switch (userSelection) {
+            case "1":
+                router.navigate("/new-book");
+                break;
+            case "2":
+                router.navigate("/books");
+            case "3":
+                System.out.println("Fare thee well!");
+                shutdown();
+                break;
+        }
     }
 }
